0b7c9952cdc0ee3371cf1538e82b2b2dac66b2f9
leon-morival <leon.morival@gmail.com>
2024-12-19 23:10:31 +0100

    footer

--- src/common/components/Footer.jsx
+++ src/common/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => (
   <footer className="bg-gray-800 text-white py-4 mt-8">
     <div className="container mx-auto text-center">
-      © {new Date().getFullYear()} Troisième rang. All rights reserved.
+      © {new Date().getFullYear()} Momostreaming. All rights reserved.
     </div>
   </footer>
 );
